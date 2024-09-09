2e298d6e9ef766946f1a343f2006b1b3078e4a64
unitlu <unitlu.official@gmail.com>
2024-09-09 11:12:00 -0500

    First commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (request: Request, response: Response) => { 
+}); 
+
+app.listen(PORT, () => { 
+  console.log("Server running at PORT: ", PORT); 
+}).on("error", (error) => {
+  throw new Error(error.message);
+});
