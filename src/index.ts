b3529eb949026e7372474910b9c63b94ea2741fa
Luis Bazán <luis@bazan.pe>
2024-09-17 16:16:35 -0500

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-import { sequelize } from './Connection/database';
-import dotenv from 'dotenv';
-import express from 'express';
-import Users from './Models/Users';
-import Sessions from './Models/Sessions';
-import User_Statistics from './Models/User_Statistics';
-import Registros from './Models/Registros';
-import Error_Keys from './Models/Error_Keys';
-import Languages from './Models/Languages';
+import { sequelize } from "./Connection/database";
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import Users from "./Models/Users";
+import Sessions from "./Models/Sessions";
+import User_Statistics from "./Models/User_Statistics";
+import Registros from "./Models/Registros";
+import Error_Keys from "./Models/Error_Keys";
+import Languages from "./Models/Languages";
 
-import Api from './Routes/api';
+import Api from "./Routes/api";
 
 dotenv.config();
 
@@ -16,20 +17,21 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 
-app.use('/api/v1/', Api);
+app.use("/api/v1/", Api);
 
 const startServer = async () => {
   try {
     await sequelize.authenticate();
-    console.log('Conexión exitosa a la base de datos.');
+    console.log("Conexión exitosa a la base de datos.");
     await sequelize.sync({ force: true });
-    console.log('Tablas sincronizadas con la base de datos.');
+    console.log("Tablas sincronizadas con la base de datos.");
     app.listen(PORT, () => {
       console.log(`Servidor corriendo en el puerto ${PORT}`);
     });
   } catch (error) {
-    console.error('Error conectándose a la base de datos:', error);
+    console.error("Error conectándose a la base de datos:", error);
   }
 };
 
