ba2368c7c07a3bafec24bb3224da81a64631cdbe
Luis Bazán <luis@bazan.pe>
2024-09-17 16:46:10 -0500

    Fix: Added first '/' into routes

--- src/Routes/api.ts
+++ src/Routes/api.ts
@@ -10,16 +10,16 @@ import { saveStats } from '../Controllers/GameController';
 
 var router = express.Router();
 
-router.post('authenticate', login);
+router.post('/authenticate', login);
 
-router.get('data/get', isAuthenticate, getData);
+router.get('/data/get', isAuthenticate, getData);
 
-router.post('session/save', isAuthenticate, saveSession);
+router.post('/session/save', isAuthenticate, saveSession);
 
-router.post('record/save', isAuthenticate, saveRegistro);
+router.post('/record/save', isAuthenticate, saveRegistro);
 
-router.post('error/save', isAuthenticate, saveErrorKeys);
+router.post('/error/save', isAuthenticate, saveErrorKeys);
 
-router.post('stats/save', isAuthenticate, saveStats);
+router.post('/stats/save', isAuthenticate, saveStats);
 
 export default router;
